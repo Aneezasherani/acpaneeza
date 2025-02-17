b6f644c723d01f0c2416ceb3322bd3911c931845
unknown <Aneezasherani>
2025-02-17 16:44:03 +0500

    first code

---
+++ 1.js
@@ -0,0 +1,14 @@
+let objArr = [
+    {
+        name: 'bmw',
+        model: '2012',
+    },
+    {
+        name: 'civic',
+        model: '2009'
+    }
+]
+objArr.map(cars);
+function cars(a) {
+    console.log(1)
+}
